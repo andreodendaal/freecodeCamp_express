ccb1f921ec62cb97be70ff040f355ee1b7df3f00
andre odendaal <aodendaal.direct@gmail.com>
2018-09-15 13:34:04 -0700

    Get input from client - Query parameters

--- myApp.js
+++ myApp.js
@@ -68,7 +68,11 @@ console.log("Hello World");
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+  app.get("/name", function(req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    res.json({name: firstName + ' ' + lastName});
+  });
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
